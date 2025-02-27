4e8b2e3b8b3a0f807731ffda44f753641ba85f6d
MariaEduardaBiedz <129293855+MariaEduardaBiedz@users.noreply.github.com>
2025-02-27 10:22:59 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    
+  );
+}
+
+function Header()
+{
+  return(
+    <div>
+      <h2>Bem vindo ao app Doceria do Padu</h2>
+       <p>Aqui você encontra doces maravilhos e a sua nostalgia vem a todo vapor, além de atendimento personalizado.</p>
+    </div>
+
+  )
+}
+
+
+
+export default App;
